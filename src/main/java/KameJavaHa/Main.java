57a736b3bbe7664729ffe6099dd149c1d7315dea
Ferran <fdiezr@uoc.es>
2024-04-04 15:45:40 +0200

    primer commit

--- src/main/java/KameJavaHa/Main.java
+++ src/main/java/KameJavaHa/Main.java
@@ -1,32 +1,48 @@
 package KameJavaHa;
 import java.sql.*;
-import java.sql.Connection;
+import java.util.Scanner;
 
 public class Main {
+    private Scanner teclado = new Scanner(System.in);
+    private MySQLJugador mySQLJugador;
 
+    public Main(Connection conn) {
+        this.mySQLJugador = new MySQLJugador(conn);
+    }
 
-    public static void main(String[] args) throws DAOException {
-        Connection conn = null;
+    public static void main(String[] args) {
         try {
-
-            System.out.println("Hello world!");
-            MySQLJugador mySQLJugador = new MySQLJugador(conn);
-            Jugador nuevoJugador = new Jugador("ferran", "Diez", 10, 15.5);
-            mySQLJugador.insertar(nuevoJugador);
-        }catch (DAOException e){
-            throw new DAOException("Error al insertar el jugador." + e.getLocalizedMessage());
-        }finally {
-            if(conn == null){
-                try{
-                    conn.close();
-                }catch(SQLException e){
-                    throw new DAOException("Error");
+            Connection conn = null;
+            try {
+                System.out.println("Hello world!");
+                Main main = new Main(conn);
+                main.insertarSocio();
+            } finally {
+                if (conn != null) {
+                    try {
+                        conn.close();
+                    } catch (SQLException e) {
+                        System.err.println("Error al cerrar la conexión: " + e.getMessage());
+                    }
                 }
             }
+        } catch (DAOException e) {
+            System.err.println("Error al insertar el jugador: " + e.getMessage());
         }
+    }
 
 
-
-
+    public void insertarSocio() throws DAOException {
+        System.out.println("introduce nombre");
+        String nombre = teclado.nextLine();
+        System.out.println("introduce apellidos");
+        String apellidos = teclado.nextLine();
+        System.out.println("Introduce dorsal");
+        int dorsal = teclado.nextInt();
+        System.out.println("Introduce puntos de media");
+        double puntosMedia = teclado.nextDouble();
+        teclado.nextLine(); // Limpiar el buffer de entrada
+        Jugador nuevoJugador = new Jugador(nombre, apellidos, dorsal, puntosMedia);
+        mySQLJugador.insertar(nuevoJugador);
     }
 }
